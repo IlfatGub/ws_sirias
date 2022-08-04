f2a88aa0d5836379318441924436800d9edb5548
ilfat <ilfat.gubaidullin@gmail.com>
2022-08-04 14:19:10 +0500

    validationKey

--- public/config/web.php
+++ public/config/web.php
@@ -24,7 +24,7 @@ $config = [
 	'components' => [
 		'request' => [
 			// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-			'cookieValidationKey' => 'jw7bvlh4LiBbrHX8KX82IB83xKur34Fl',
+			'cookieValidationKey' => getenv('cookieValidationKey_sirias'),
 		],
 		'cache' => [
 			'class' => 'yii\caching\FileCache',
