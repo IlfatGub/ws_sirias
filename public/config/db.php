81182f92e9fc2c0bc540d23ecb3ecb889fc4a41e
ilfat <ilfat.gubaidullin@gmail.com>
2022-08-04 14:14:20 +0500

    env

--- public/config/db.php
+++ public/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sirias',
+    'dsn' => 'mysql:host='.getenv('SQL_IP').';dbname=sirias',
     'username' => 'root',
-    'password' => 'armagedon',
+    'password' => getenv('SQL_ROOT_PASSWORD'),
     'charset' => 'utf8',
 ];
